2e39bd6052d0883c8bd0b47664d65e178b189f6e
Muhammad Usman <muh.musman@gmail.com>
2025-01-22 10:14:23 +0500

    jazzcash done

--- first.js
+++ first.js
@@ -10,4 +10,10 @@ console.log(footer);
 =======
 const payment = "integrate the payment added";
 console.log(payment);
+<<<<<<< HEAD
 >>>>>>> faa1411 (payment added)
+=======
+
+const jazzcash = "integrate the jazzcash as well";
+console.log(jazzcash);
+>>>>>>> 9b2a9f5 (jazzcash added)
