ad1361c096e4fa6ca4c92aa287011a5756dbd299
Muhammad Usman <muh.musman@gmail.com>
2025-01-22 10:11:20 +0500

    payment updated after footer

--- first.js
+++ first.js
@@ -4,5 +4,10 @@ console.log(button);
 const login = "Login page Added";
 console.log(login);
 
+<<<<<<< HEAD
 const footer = "footer added in our website";
 console.log(footer);
+=======
+const payment = "integrate the payment added";
+console.log(payment);
+>>>>>>> faa1411 (payment added)
